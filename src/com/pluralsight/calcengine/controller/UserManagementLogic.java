6982411aa147f99617312ec429103a03fd0ecc48
hendrybones <hendrymwamburi56@gmail.com>
2021-08-24 07:22:08 +0300

    creating db user&book object class

--- src/com/pluralsight/calcengine/controller/UserManagementLogic.java
+++ src/com/pluralsight/calcengine/controller/UserManagementLogic.java
@@ -7,5 +7,18 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class UserManagementLogic {
+    public static boolean CheckIfAlready(String UserName)
+    {
+        boolean status=false;
+        try{
+            Connection con=DbUtil.getConnection();
+            PreparedStatement ps=con.prepareStatement("select * from Users where UserName=?");
+            ps.setString(1,UserName);
+            ResultSet rs=ps.executeQuery();
+            status=rs.next();
+            con.close();
+        }catch(Exception e){System.out.println(e);}
+        return status;
 
+    }
 }
